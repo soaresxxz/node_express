a8ada299dc44b618eb73a05fe325771af729a45e
Mateus Soares <mateus.soares_2013@hotmail.com>
2023-11-06 15:46:38 -0300

    utilizaçao do nodemon para reutilizaçao do projeto

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+
+const caminhoBase= path.join(__dirname , "templates") 
+
+app.get('/' , (requisao , resposta) =>{
+    resposta.sendFile(`${caminhoBase}/index.html`)
+    
+})
+
+app.listen(3000 ,() =>{
+    console.log("Servidor rodando na porta 3000!")
+})
+ 
